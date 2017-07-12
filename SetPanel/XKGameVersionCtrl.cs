40913e77aa6fac3a0a5c2dd5be8110f8bad736a1
syq <shenyongqiang2008@126.com>
2017-07-12 16:35:49 +0800

    更新游戏版本号

--- SetPanel/XKGameVersionCtrl.cs
+++ SetPanel/XKGameVersionCtrl.cs
@@ -8,9 +8,12 @@ public class XKGameVersionCtrl : MonoBehaviour {
 	 * 1.处理第四关玩家合体时,如果游戏进入倒计时,画面会停止运动的bug.
 	 * 
 	 * #########################################################
-	 * Version: V1.0_20170705战火雄心-坦克直升机联合作战:
+	 * Version: V1.1_20170712战火雄心-坦克直升机联合作战:
+	 * 1.添加机台气囊控制逻辑.
+	 * 2.修改游戏难度.
+	 * 3.添加机台硬件测试工具.
 	 */
-	public static string GameVersion = "Version: V1.0_20170705";
+	public static string GameVersion = "Version: V1.1_20170712";
 	// Use this for initialization
 	void Start()
 	{
